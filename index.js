e25293248a7766fd6ec0064d900f619782e07358
AHMEDHAZEM-13 <ahmedhazem635376@gail.com>
2024-08-17 22:57:15 +0300

    init

---
+++ index.js
@@ -0,0 +1,161 @@
+// 1
+
+// const num = +prompt("enter number")
+
+// if (num ==100) {
+// console.log("equal to 100");
+    
+// }else if (num >100) {
+//     console.log("greater than 100");
+    
+// }else  (num <100) 
+
+//     console.log(" less than 100" );
+
+// 2-:
+
+// const text = +prompt("enter text")
+
+// const exampleText = "Your text goes here. Replace this with any string to test.";
+// checkStringLength(exampleText);
+
+// function checkStringLength(text) {
+//     if (text.length > 100) {
+//         console.log("The string is long.");
+//     } else {
+//         console.log("The string is short.");
+//     }
+// }
+
+
+// 3-:
+// const text = +prompt("enter password")
+// const password = "ahmed123"; 
+// const confirmPassword = "ahmed123"; 
+// validatePassword(password, confirmPassword);
+
+
+// function validatePassword(password, confirmPassword) {
+//     if (password === confirmPassword) {
+//         console.log("Password is valid.");
+        
+//     } else {
+//         console.log("Password is not valid.");
+      
+//     }
+// }
+
+
+// 4--:
+
+// var day = "x"
+// var x = "saterday"
+// var day = +prompt("enter day")
+// if (x=true) {
+//     console.log("It's weekend");
+    
+    
+// } else {
+//     console.log("work day");
+// }
+
+// 5
+// var num = +prompt("enter number")
+// if (num>0) {
+//     console.log("positive Number");
+// }
+// else if (num<0) {
+//     console.log("negative Number");
+    
+ 
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+//  6-:   
+// const score = +prompt("enter score")
+    
+// const grade = getGrade(score);
+// console.log(`The grade for a score of ${score} is: ${grade}`);
+// function getGrade(score) {
+//     if (score < 50) {
+//         return 'F';
+//     } else if (score >= 50 && score < 60) {
+//         return 'D';
+//     } else if (score >= 60 && score < 70) {
+//         return 'C';
+//     } else if (score >= 70 && score < 80) {
+//         return 'B';
+//     } else {
+//         return 'A'; 
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// 7-:
+
+// const temperature = +prompt("enter password")
+// const deg = 0; 
+
+// if (temperature > 30) {
+//     console.log("It's hot");
+// } else {
+//     console.log("It's cold");
+// }
+
+
+// 8-:
+
+// const number = +prompt("enter number")
+// if (number % 2 === 0) {
+//     console.log(" even number");
+// } else {
+//     console.log("  odd number");
+// }
+
+
+// 9-:
+// const number= +prompt("enter number")
+
+
+// if (number > 20) {
+//     if (number % 2 === 0) {
+//         console.log("You can enter the university");
+//     } else {
+//         console.log(" even number");
+//     }
+// } if (number > 20) {
+//     if (number % 2 === 1) {
+//         console.log("You can't enter");
+//     }
+// } else {
+//     console.log("odd number");
+// }
+
+
+
+    var result = confirm("Do you want to proceed?");
+    
+    if (result) {
+        // User clicked "OK"
+    } else {
+        // User clicked "Cancel"
+    }
+  
